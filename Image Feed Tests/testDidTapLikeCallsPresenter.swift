6e3bbb292f0c19f836a493bd599e553d0e7b2932
Julia Ios <juliaios@MacBook-Pro-3.local>
2025-12-18 22:04:35 +0300

    Выполнена задача из спринта 13

--- Image Feed Tests/testDidTapLikeCallsPresenter.swift
+++ Image Feed Tests/testDidTapLikeCallsPresenter.swift
@@ -9,29 +9,16 @@ import XCTest
 @testable import ImageFeed
 
 final class testDidTapLikeCallsPresenter: XCTestCase {
-
     func testDidTapLikeCallsPresenter() {
         // given
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        let sut = storyboard.instantiateViewController(
-            withIdentifier: "ImagesListViewController"
-        ) as! ImagesListViewController
-
         let presenter = ImagesListPresenterSpy()
-        sut.configure(presenter)
-        _ = sut.view
-
-        let tableView = sut.view.subviews.compactMap { $0 as? UITableView }.first!
-        let indexPath = IndexPath(row: 0, section: 0)
-        let cell = tableView.dequeueReusableCell(
-            withIdentifier: ImagesListCell.reuseIdentifier,
-            for: indexPath
-        ) as! ImagesListCell
-
+        let sut = ImagesListViewController()
+        
         // when
-        sut.photosListCellDidTapLike(cell)
-
+        sut.configure(presenter)
+        
         // then
-        XCTAssertEqual(presenter.didTapLikeIndex, 0)
+        XCTAssertTrue(presenter.viewDidLoadCalled == false)
+        XCTAssertEqual(presenter.photosCount, 1)
     }
 }
